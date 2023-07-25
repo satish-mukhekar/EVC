2cfe38bc9305e14f953ab523d14c9d466c571703
Satish Mukhekar <126197329+satish-mukhekar@users.noreply.github.com>
2023-07-25 10:21:34 +0530

    Update EVCNftStake.sol

--- EVCNftStake.sol
+++ EVCNftStake.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
+pragma solidity ^ 0.8 .0;
 
 import "./Context.sol";
 import "./Ownable.sol";
@@ -13,8 +12,6 @@ interface Token {
 
     function transfer(address recipient, uint256 amount) external returns(bool);
 
-    function transferFrom(address spender, address recipient, uint amount) external returns(bool);
-
     function balanceOf(address account) external view returns(uint256);
 
 }
@@ -24,20 +21,31 @@ interface NFT {
 
     function transferFrom(address from, address to, uint256 tokenId) external;
 
-    function getMyReferrer(address _user) external view returns(address);
-
 }
 
 
 contract StakeNFT is Pausable, Ownable, ReentrancyGuard {
-
     Token erc20Token;
     NFT nftToken;
 
-    address admin;
-
     uint256 public planCount;
     uint256 public claimTime = 60;
+
+    /* planId => plan mapping */
+    mapping(uint256 => Plan) public plans;
+    /* tokenId => token info */
+    mapping(uint256 => TokenInfo) public tokenInfos;
+    // Mapping owner address to stake token count
+    mapping(address => uint256) public userStakeCnt;
+    // Mapping from token ID to staker address
+    mapping(uint256 => address) public stakers;
+    /* address->array index->tokenId */
+    mapping(address => mapping(uint256 => uint256)) stakedTokens;
+    // Mapping from token ID to index of the owner tokens list
+    mapping(uint256 => uint256) stakedTokensIndex;
+    //maping for address=> tokenid => claim_timing
+    mapping(address => mapping(uint256 => uint256)) public nextClaimTime;
+    mapping(address => mapping(uint256 => uint256)) lastClaimTime;
 
     struct Plan {
         uint256 rewardBal;
@@ -63,26 +71,7 @@ contract StakeNFT is Pausable, Ownable, ReentrancyGuard {
     event Staked(address indexed from, uint256 planId, uint256[] _ids);
     event UnStaked(address indexed from, uint256[] _ids);
     event Claimed(address indexed from, uint256[] _ids, uint256 amount);
-    event RewardPercentage(uint256 rewardPercentage, uint256 reward, uint totalreward);
-    event reffralRewardAmount(uint256 directReward, uint indirectreward);
-    event directReferralRewardTransferred(address admin, address directReferrer, uint rewardAmount);
-    event indirectReferralRewardTransferred(address admin, address indirectReferrer, uint rewardAmount);
-
-    /* planId => plan mapping */
-    mapping(uint256 => Plan) public plans;
-    /* tokenId => token info */
-    mapping(uint256 => TokenInfo) public tokenInfos;
-    // Mapping owner address to stake token count
-    mapping(address => uint256) public userStakeCnt;
-    // Mapping from token ID to staker address
-    mapping(uint256 => address) public stakers;
-    /* address->array index->tokenId */
-    mapping(address => mapping(uint256 => uint256)) stakedTokens;
-    // Mapping from token ID to index of the owner tokens list
-    mapping(uint256 => uint256) stakedTokensIndex;
-    //maping for address=> tokenid => claim_timing
-    mapping(address => mapping(uint => uint)) public nextClaimTime;
-    mapping(address => mapping(uint => uint)) lastClaimTime;
+    event RewardPercentage(uint256 rewardPercentage, uint256 reward, uint256 totalreward);
 
     //Constructor
     constructor(Token _tokenAddress, NFT _nfttokenAddress) {
@@ -90,7 +79,6 @@ contract StakeNFT is Pausable, Ownable, ReentrancyGuard {
         require(address(_nfttokenAddress) != address(0), "NFT Token Address cannot be address 0");
         erc20Token = _tokenAddress;
         nftToken = _nfttokenAddress;
-        admin = Ownable.owner();
     }
 
     //User
@@ -125,8 +113,6 @@ contract StakeNFT is Pausable, Ownable, ReentrancyGuard {
     function claimReward(uint256[] calldata _ids) public nonReentrant {
         require(_ids.length > 0, "invalid arguments");
         uint256 totalClaimAmt = 0;
-        uint directReffralReward;
-        uint indirectReffralReward;
         for (uint256 i = 0; i < _ids.length; i++) {
             require(stakers[_ids[i]] == _msgSender(), "NFT does not belong to sender address");
             require(nextClaimTime[msg.sender][_ids[i]] < block.timestamp, "wait till next claimable timing");
@@ -139,29 +125,16 @@ contract StakeNFT is Pausable, Ownable, ReentrancyGuard {
             }
             uint256 reward = (getUnClaimedReward(_ids[i]) * rewardPercentage) / 100;
             require(reward > 0, "You have no rewards to claim.");
-            totalClaimAmt = reward; // ading reward to totalclaimamount
-            directReffralReward = totalClaimAmt / 10;
-            indirectReffralReward = totalClaimAmt * 5 / 100;
-            emit reffralRewardAmount(directReffralReward, indirectReffralReward); //can be removed after final
+            totalClaimAmt = reward; // ading reward to totalclaimamount     
             //only for developer use             
-            uint rewardacctualpercentage = ((totalClaimAmt * 100) / getUnClaimedReward(_ids[i]));
+            uint256 rewardacctualpercentage = ((totalClaimAmt * 100) / getUnClaimedReward(_ids[i]));
             emit RewardPercentage(rewardacctualpercentage, totalClaimAmt, getUnClaimedReward(_ids[i]));
             tokenInfos[_ids[i]].claimed += reward;
             lastClaimTime[msg.sender][_ids[i]] = block.timestamp;
             nextClaimTime[msg.sender][_ids[i]] = block.timestamp + claimTime;
         }
-        address directReferrer = nftToken.getMyReferrer(msg.sender);
-        address indirectReferrer = nftToken.getMyReferrer(directReferrer);
         require(totalClaimAmt > 0, "Claim amount invalid.");
         require(erc20Token.transfer(_msgSender(), totalClaimAmt), "Token transfer failed!");
-        if (directReferrer != address(0)) {
-            require(erc20Token.transferFrom(admin, directReferrer, directReffralReward), "Token transfer to Direct Referrer failed!");
-            emit directReferralRewardTransferred(admin, directReferrer, directReffralReward);
-        }
-        if (indirectReferrer != address(0)) {
-            require(erc20Token.transferFrom(admin, indirectReferrer, indirectReffralReward), "Token transfer to Indirect Referrer failed!");
-            emit indirectReferralRewardTransferred(admin, indirectReferrer, indirectReffralReward);
-        }
         emit Claimed(_msgSender(), _ids, totalClaimAmt);
     }
 
@@ -185,8 +158,29 @@ contract StakeNFT is Pausable, Ownable, ReentrancyGuard {
     }
 
     //View
-    function getRewardPercentage(address _user, uint _id) public view returns(uint) {
-        if (stakers[_id] == address(0)) {
+    function getCurrentAPR(uint256 planId) public view returns(uint256) {
+        require(plans[planId].rewardBal > 0, "Invalid staking plan");
+        uint256 perNFTShare;
+        uint256 stakingBucket = plans[planId].rewardBal;
+        uint256 currstakeCount = plans[planId].currCount == 0 ? 1 : plans[planId].currCount; // avoid divisible by 0 error
+        uint256 maxNFTShare = (currstakeCount * plans[planId].perNFTPrice * plans[planId].maxApyPer) / 100;
+        if (maxNFTShare < stakingBucket)
+            perNFTShare = maxNFTShare / currstakeCount;
+        else perNFTShare = stakingBucket / currstakeCount;
+        return (perNFTShare * 100) / plans[planId].perNFTPrice;
+    }
+
+    //only for developer use // MAKE IT ZERO FOR OTHER USER WHO HAVE NOT STAKED IT, IF IT WANT TO MAKE AVILABLE FOR USER IT IS SHOWING TIMESTAMP VALUE FOR THE OTHER USER
+    function getElapsedTime(address _user, uint256 _id) public view returns(uint256) {
+        if (stakers[_id] != _user) {
+            return 0;
+        }
+        uint256 elapsedTime = block.timestamp - lastClaimTime[_user][_id];
+        return elapsedTime;
+    }
+
+    function getRewardPercentage(address _user, uint256 _id) public view returns(uint256) {
+        if (stakers[_id] != _user) {
             return 0;
         }
         uint256 elapsedTime = block.timestamp - lastClaimTime[_user][_id];
@@ -218,37 +212,16 @@ contract StakeNFT is Pausable, Ownable, ReentrancyGuard {
         return rewardPercentage;
     }
 
-    //only for developer use // MAKE IT ZERO FOR OTHER USER WHO HAVE NOT STAKED IT, IF IT WANT TO MAKE AVILABLE FOR USER IT IS SHOWING TIMESTAMP VALUE FOR THE OTHER USER
-    function getElapsedTime(address _user, uint256 _id) public view returns(uint256) {
-        if (stakers[_id] == address(0)) {
-            return 0;
-        }
-        uint256 elapsedTime = block.timestamp - lastClaimTime[_user][_id];
-        return elapsedTime;
-    }
-
-    function getCurrentAPY(uint256 planId) public view returns(uint256) {
-        require(plans[planId].rewardBal > 0, "Invalid staking plan");
-        uint256 perNFTShare;
-        uint256 stakingBucket = plans[planId].rewardBal;
-        uint256 currstakeCount = plans[planId].currCount == 0 ? 1 : plans[planId].currCount; // avoid divisible by 0 error
-        uint256 maxNFTShare = (currstakeCount * plans[planId].perNFTPrice * plans[planId].maxApyPer) / 100;
-        if (maxNFTShare < stakingBucket)
-            perNFTShare = maxNFTShare / currstakeCount;
-        else perNFTShare = stakingBucket / currstakeCount;
-        return (perNFTShare * 100) / plans[planId].perNFTPrice;
-    }
-
     function getUnClaimedReward(uint256 tokenId) public view returns(uint256) {
         require(tokenInfos[tokenId].startTS > 0, "Token not staked");
-        uint256 apy;
+        uint256 apr;
         uint256 anualReward;
         uint256 perSecondReward;
         uint256 stakeSeconds;
         uint256 reward;
         uint256 matureTS;
-        apy = getCurrentAPY(tokenInfos[tokenId].planId);
-        anualReward = (plans[tokenInfos[tokenId].planId].perNFTPrice * apy) / 100;
+        apr = getCurrentAPR(tokenInfos[tokenId].planId);
+        anualReward = (plans[tokenInfos[tokenId].planId].perNFTPrice * apr) / 10000;
         perSecondReward = anualReward / (365 * 86400);
         matureTS = tokenInfos[tokenId].startTS + plans[tokenInfos[tokenId].planId].expireSeconds;
         if (tokenInfos[tokenId].endTS == 0)
@@ -259,7 +232,7 @@ contract StakeNFT is Pausable, Ownable, ReentrancyGuard {
             stakeSeconds = matureTS - tokenInfos[tokenId].startTS;
         else
             stakeSeconds = tokenInfos[tokenId].endTS - tokenInfos[tokenId].startTS;
-        uint multiplier;
+        uint256 multiplier;
         if (tokenId >= 1 && tokenId <= 10) {
             multiplier = 1;
         } else if (tokenId >= 21 && tokenId <= 30) {
@@ -323,6 +296,22 @@ contract StakeNFT is Pausable, Ownable, ReentrancyGuard {
     }
 
     //Admin
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function setClaimTime(uint256 _claimTime) public onlyOwner {
+        claimTime = _claimTime;
+    }
+
+    function setErc20Token(address _erc20Token) public onlyOwner {
+        erc20Token = Token(_erc20Token);
+    }
+
+    function setNftToken(address _nftToken) public onlyOwner {
+        nftToken = NFT(_nftToken);
+    }
+
     function setStakePlan(
         uint256 id,
         uint256 _rewardBal,
@@ -348,20 +337,12 @@ contract StakeNFT is Pausable, Ownable, ReentrancyGuard {
         emit StakePlan(id);
     }
 
-    function setClaimTime(uint256 _claimTime) public onlyOwner {
-        claimTime = _claimTime;
-    }
-
-    function transferToken(address to, uint256 amount) public onlyOwner {
-        require(erc20Token.transfer(to, amount), "Token transfer failed!");
-    }
-
     function transferNFT(address to, uint256 tokenId) public onlyOwner {
         nftToken.transferFrom(address(this), to, tokenId);
     }
 
-    function pause() public onlyOwner {
-        _pause();
+    function transferToken(address to, uint256 amount) public onlyOwner {
+        require(erc20Token.transfer(to, amount), "Token transfer failed!");
     }
 
     function unpause() public onlyOwner {
@@ -369,3 +350,6 @@ contract StakeNFT is Pausable, Ownable, ReentrancyGuard {
     }
 
 }
+
+
+// 1,40000000000000000000000000,100000000,113600,11360,0,157680000,100000000000000000000,157680000(Plan of testing)
